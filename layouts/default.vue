55d631ebe4c862b56b2c11e1895a43dc2f2c3dd9
Toliak <toliakpurple@gmail.com>
2021-03-03 01:29:27 +0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+:root {
+  --white: #fff;
+  --blue: #297fca;
+  --purple: #2841c9;
+  --orange: #c97328;
+  --dark-gray: #383f45;
+  --gray: #777e84;
+}
+
+body, html {
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <Title icon="IconMtasaTypescript" label="MTA:SA TypeScript"></Title>
+    <Plate></Plate>
+    <ButtonGroup></ButtonGroup>
+    <div class="links">
+<!--      <a-->
+<!--        href="https://nuxtjs.org/"-->
+<!--        target="_blank"-->
+<!--        rel="noopener noreferrer"-->
+<!--        class="button&#45;&#45;green"-->
+<!--      >-->
+<!--        Documentation-->
+<!--      </a>-->
+<!--      <a-->
+<!--        href="https://github.com/nuxt/nuxt.js"-->
+<!--        target="_blank"-->
+<!--        rel="noopener noreferrer"-->
+<!--        class="button&#45;&#45;grey"-->
+<!--      >-->
+<!--        GitHub-->
+<!--      </a>-->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+
+
+.links {
+  padding-top: 15px;
+}
+</style>
